ec4bb9d5ceb3d25b2c5183fb959c3f151d424c7f
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-05 13:44:21 -0700

    Add files via upload

---
+++ OOP1/ejercicio5_Producto.java
@@ -0,0 +1,10 @@
+//Joaquin Kaleb Chavira Gonzlez 1:40
+class ejercicio5_Producto
+{
+public static void main(String args[]){
+System.out.println("Numero 1: 50");
+System.out.println("Numero 2: 2");
+
+System.out.println("El producto es: "+ 50*2);
+}
+}
