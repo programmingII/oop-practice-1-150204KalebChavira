f2e1326bcc830b9126e8c37f8279b60b7b3769d7
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-05 13:49:16 -0700

    Add files via upload

---
+++ OOP1/ejercicio6_process.java
@@ -0,0 +1,12 @@
+//Joaquin Kaleb Chavira Gonzalez 1:45
+
+class ejercicio6_process{
+
+public static void main(String args []){
+System.out.println("Numero 1: 20\n Numero 2: 5");
+System.out.println("20+5= "20+5);
+System.out.println("20-5= "20-5);
+System.out.println("20*5= "20*5);
+System.out.println("20/5= "20/5);
+}
+}
