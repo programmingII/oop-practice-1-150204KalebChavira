e55d9b1fd9cac51a170e9c5fb6dcf97cdbfed855
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-07 12:27:17 -0700

    Add files via upload

---
+++ OOP1/ejercicio7_tablas8.java
@@ -0,0 +1,14 @@
+/*Name: Joaquin Kaleb Chavira Gonzalez
+Dia: 07/02/2019
+Hora: 12:19*/
+
+class ejercicio7_tablas8{ //inicio de la clase
+
+public static void main (String args[]){	//inicio del main
+
+for(int i=1;i<=10;i++){				//uso de un for para la repeticion de un numero
+System.out.println("8X"+i+"= "+(8*i));		//impresion del cada una de las tablas
+}						//salida del for
+
+}						//cierre del main
+}						//cierre de la clase
