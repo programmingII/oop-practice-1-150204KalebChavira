6c8f25b913b7a8c7dbee14dc0e2f3fdffbaf0a27
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-07 12:51:10 -0700

    Add files via upload

---
+++ OOP1/Ejercicio8_java.java
@@ -0,0 +1,14 @@
+/*Name: Joaquin Kaleb Chavira Gonzalez
+Dia: 07/02/2019
+Hora: 12:45*/
+
+public class Ejercicio8_java {			//inicio de clase
+ 
+    public static void main(String[] args) {		//main
+
+        System.out.println("    J    A   v     v  A ");	//impresion radical de java
+        System.out.println("J   J   A A   v   v  A A");
+        System.out.println("J   J  AAAAA   V V  AAAAA");
+        System.out.println(" JJJ  A     A   V  A     A");
+    }								//salida main
+}								//salida class
