55e1878429cc6e781343982fe3d5ecc3649655de
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-07 12:55:12 -0700

    Add files via upload

---
+++ OOP1/Ejercicio9_expresion.java
@@ -0,0 +1,13 @@
+/*Name: Joaquin Kaleb Chavira Gonzalez
+Dia: 07/02/2019
+Hora: 12:52*/
+
+public class Ejercicio9_expresion {			//inicio de clase
+ 
+public static void main(String[] args) {		//main
+
+System.out.println("((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)) ==" + ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)) );	//impresion radical de la expresion
+       
+       
+    }								//salida main
+}								//salida class
