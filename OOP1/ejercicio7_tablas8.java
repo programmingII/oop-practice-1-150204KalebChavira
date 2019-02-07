99ae084990b012b543f1b697193f57d2171fae39
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-07 12:42:16 -0700

    Add files via upload

--- OOP1/ejercicio7_tablas8.java
+++ OOP1/ejercicio7_tablas8.java
@@ -2,13 +2,21 @@
 Dia: 07/02/2019
 Hora: 12:19*/
 
-class ejercicio7_tablas8{ //inicio de la clase
+import java.util.Scanner;			//libreria del scanner para obtener datos de teclado
+
+class ejercicio7_tablas8{ 			//inicio de la clase
 
 public static void main (String args[]){	//inicio del main
 
+int numero;
+
+Scanner sc = new Scanner(System.in);		//creacion del objeto scanner
+System.out.println("Ingresa el numero a multiplicar\n");
+numero = sc.nextInt();			//ingreso del numero a multiplicar
+
 for(int i=1;i<=10;i++){				//uso de un for para la repeticion de un numero
-System.out.println("8X"+i+"= "+(8*i));		//impresion del cada una de las tablas
+System.out.println(numero +"X"+i+"= "+(numero*i));		//impresion del cada una de las tablas
 }						//salida del for
 
 }						//cierre del main
-}						//cierre de la clase
+}
