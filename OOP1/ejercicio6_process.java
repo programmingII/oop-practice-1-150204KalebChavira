19f0efe5c8647813c665c450d8e2e8ba4fbe0da6
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-07 12:14:35 -0700

    Update ejercicio6_process.java

--- OOP1/ejercicio6_process.java
+++ OOP1/ejercicio6_process.java
@@ -1,12 +1,13 @@
+
 //Joaquin Kaleb Chavira Gonzalez 1:45
 
 class ejercicio6_process{
 
 public static void main(String args []){
-System.out.println("Numero 1: 20\n Numero 2: 5");
-System.out.println("20+5= "20+5);
-System.out.println("20-5= "20-5);
-System.out.println("20*5= "20*5);
-System.out.println("20/5= "20/5);
+System.out.println("Numero 1: 20\nNumero 2: 5");
+System.out.println("20+5= "+(20+5));
+System.out.println("20-5= "+(20-5));
+System.out.println("20*5= "+(20*5));
+System.out.println("20/5= "+(20/5));
 }
 }
