f5bc4bbcce949ce10f9b7af72ad5ca3a42e08ea0
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-07 12:59:01 -0700

    Add files via upload

---
+++ OOP1/Ejercicio10_Formula.java
@@ -0,0 +1,13 @@
+/*Name: Joaquin Kaleb Chavira Gonzalez
+Dia: 07/02/2019
+Hora: 12:56*/
+
+public class Ejercicio10_Formula {			//inicio de clase
+ 
+public static void main(String[] args) {		//main
+
+System.out.println("4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)) ==" + 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)) );	//impresion radical de la expresion
+       
+       
+    }								//salida main
+}								//salida class
