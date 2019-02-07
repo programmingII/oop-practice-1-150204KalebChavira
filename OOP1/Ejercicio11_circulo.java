45d9ff1737b8190b2c96089f44167d6f5d78330f
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-07 13:18:47 -0700

    Add files via upload

---
+++ OOP1/Ejercicio11_circulo.java
@@ -0,0 +1,16 @@
+/*Name: Joaquin Kaleb Chavira Gonzalez
+Dia: 07/02/2019
+Hora: 12:56*/
+
+public class Ejercicio11_circulo {			//inicio de clase
+ 
+public static void main(String[] args) {		//main
+double pi;
+double r;
+pi = 3.1416;		//asignacion de pi
+r = 7.5 ;			//asiganacion del radio
+System.out.println("Perimetro: "+ (2*pi)*r);		//salida del perimetro
+System.out.println("El radio : "+ (pi)*r*r);		//salida del area
+       
+    }								//salida main
+}								//salida class
