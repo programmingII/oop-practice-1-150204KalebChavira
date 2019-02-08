6bfbf08e8f852a1213684c25f5a2fc00c524c040
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-08 15:38:32 -0700

    Update Ejercicio12_promedio.java

--- OOP1/Ejercicio12_promedio.java
+++ OOP1/Ejercicio12_promedio.java
@@ -7,7 +7,7 @@ import java.util.Scanner;			//importar la libreria Scanner
 public class Ejercicio12_promedio {			//inicio de clase
  
 public static void main(String[] args) {		//main
-double a,b,c;
+double a,b,c,d,e;
 double res;
 Scanner sc= new Scanner(System.in); 			//Creacion del objeto Scanner
 
@@ -19,8 +19,14 @@ b=sc.nextInt();						//Escaneo del dato B
 
 System.out.println("Ingresa el nuemero 3\n");		//Mensaje para ingresar un datoC
 c=sc.nextInt();       					//Escaneo del dato C
+ 
+System.out.println("Ingresa el nuemero 4\n");		//Mensaje para ingresar un datoC
+d=sc.nextInt();       					//Escaneo del dato d
 
-res=(a+b+c)/3;
+System.out.println("Ingresa el nuemero 5\n");		//Mensaje para ingresar un datoC
+e=sc.nextInt();       					//Escaneo del dato e 
+ 
+res=(a+b+c+d+e)/5;  //aqui se hace la sumatoria
 System.out.println("El promedio es= " + res);
 }								//salida main
 }								//salida class
