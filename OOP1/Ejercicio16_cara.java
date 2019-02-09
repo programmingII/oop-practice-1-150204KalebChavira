67db954c16744f2f468e229140b80c789fe0c7ec
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-08 17:20:04 -0700

    Add files via upload

---
+++ OOP1/Ejercicio16_cara.java
@@ -0,0 +1,15 @@
+/*Name: Joaquin Kaleb Chavira Gonzalez
+Dia: 08/02/2019
+Hora: 4:20*/
+
+
+public class Ejercicio16_cara {			//inicio de clase
+ 
+public static void main(String[] args) {		//main
+System.out.println("/////////");
+System.out.println("0| o o |0");
+System.out.println(" |  L  | ");
+System.out.println(" |  O  | ");
+System.out.println("  _____ ");
+}								//salida main
+}		
