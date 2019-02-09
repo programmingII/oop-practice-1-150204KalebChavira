d35086bdb1a279e9e7063baa6dee3aaf2ec88f1b
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-08 17:14:58 -0700

    Add files via upload

---
+++ OOP1/Ejercicio15_cambiova.java
@@ -0,0 +1,18 @@
+/*Name: Joaquin Kaleb Chavira Gonzalez
+Dia: 08/02/2019
+Hora: 4:20*/
+
+
+public class Ejercicio15_cambiova {			//inicio de clase
+ 
+public static void main(String[] args) {		//main
+   int a, b, cambio;
+   a = 1;
+   b = 2;
+   System.out.println("Antes del cambio: a, b = "+a+", "+ + b);
+   cambio = a;
+   a = b;
+   b = cambio;   
+  System.out.println("Despues del cambio : a, b = "+a+", "+ + b);
+}								//salida main
+}		
