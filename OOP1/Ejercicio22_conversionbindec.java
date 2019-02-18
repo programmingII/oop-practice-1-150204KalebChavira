797a8b4c1090301d94278779b8c4678e41e8b56e
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-17 17:33:01 -0700

    Add files via upload

---
+++ OOP1/Ejercicio22_conversionbindec.java
@@ -0,0 +1,18 @@
+/*Name: Joaquin Kaleb Chavira Gonzalez
+Date: 17/02/19
+Hour: 5:20*/
+import java.util.Scanner; //Importamos la libreria Scanner
+
+public class Ejercicio22_conversionbindec{ 				//declaracion de la clase 
+public static void main (String [ ] args){			//declaracion del programa principal 
+
+Scanner sc=new Scanner(System.in); //se crea el objeto de la clase Scanner para poder capturar datos desde el teclado
+String bin;
+int n1;
+
+System.out.println("Ingrese el numero binario: "); //se imprime en pantalla la instruccion
+bin=sc.nextLine(); //se le asigna el valor 
+
+System.out.println("El numero decimal es: " + Integer.parseInt(bin,2)); //Aqui mismo se hace la convercion y se imprime
+}
+}
