c3f78158037f8a590fff8dee4ae227b1834ef10e
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-17 19:00:50 -0700

    Update Ejercicio24_conversionbinoctal.java

--- OOP1/Ejercicio24_conversionbinoctal.java
+++ OOP1/Ejercicio24_conversionbinoctal.java
@@ -7,15 +7,15 @@ public class Ejercicio24_conversionbinoctal{ 				//declaracion de la clase
 public static void main (String [ ] args){			//declaracion del programa principal 
 
 Scanner sc=new Scanner(System.in); //se crea el objeto de la clase Scanner para poder capturar datos desde el teclado
-String bin,oct;
-int middle;
+String bin,oct;   //variables ha transformar
+int middle;   //variable de punto medio en decimal
 
-System.out.println("Ingrese el numero binario: "); //se imprime en pantalla la instruccion
+System.out.println("Ingrese el numero binario: "); //se pide un numero binario
 bin = sc.nextLine();		//capturamos el dato del binario
 
 middle=Integer.parseInt(bin,2); 		//se convierte del numero binario a decimal
 oct=Integer.toOctalString(middle);		//aqui convertimos del decimal al octal
 
-System.out.println("El numero Octal es: "+ oct); //Aqui mismo se hace la convercion y se imprime
+System.out.println("El numero Octal es: "+ oct); //Aqui se imprime el octal
 }
 }
