10b79212afe9202b1c9b27d8640b91fa5ec9e4d0
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-17 19:08:08 -0700

    Update Ejercicio28_convhexdec.java

--- OOP1/Ejercicio28_convhexdec.java
+++ OOP1/Ejercicio28_convhexdec.java
@@ -7,15 +7,15 @@ public class Ejercicio28_convhexdec{ 				//declaracion de la clase
 public static void main (String [ ] args){			//declaracion del programa principal 
 
 Scanner sc=new Scanner(System.in); //se crea el objeto de la clase Scanner para poder capturar datos desde el teclado
-String hex;
-int middle;
+String hex;//var hexadecimal
+int middle; //var decimal
 
-System.out.println("Ingrese el numero hexadecimal: "); //se imprime en pantalla la instruccion
-hex= sc.nextLine();		//capturamos el dato del binario
+System.out.println("Ingrese el numero hexadecimal: "); //se pide el hexadecimal
+hex= sc.nextLine();		//capturamos el dato del hexadecimal
 
-middle=Integer.parseInt(hex,16); 		//se convierte del numero binario a decimal
-//hex=Integer.toHexString(middle);		//aqui convertimos del decimal al octal
+middle=Integer.parseInt(hex,16); 		//se convierte del numero hexa a decimal
 
-System.out.println("El numero decimal es: "+ middle); //Aqui mismo se hace la convercion y se imprime
+
+System.out.println("El numero decimal es: "+ middle); //Aqui se imprime el decimal
 }
 }
