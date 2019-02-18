b6fabe45ef7ab47dcd76ce5428a683b6713d49b8
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-17 19:06:56 -0700

    Update Ejercicio27_convocthex.java

--- OOP1/Ejercicio27_convocthex.java
+++ OOP1/Ejercicio27_convocthex.java
@@ -7,15 +7,15 @@ public class Ejercicio27_convocthex{ 				//declaracion de la clase
 public static void main (String [ ] args){			//declaracion del programa principal 
 
 Scanner sc=new Scanner(System.in); //se crea el objeto de la clase Scanner para poder capturar datos desde el teclado
-String oct,hex;
-int middle;
+String oct,hex;   //se declara las conversiones
+int middle;       // punto medio para almacenar en decimal
 
-System.out.println("Ingrese el numero octal: "); //se imprime en pantalla la instruccion
-oct = sc.nextLine();		//capturamos el dato del binario
+System.out.println("Ingrese el numero octal: "); //se pide el numero octal
+oct = sc.nextLine();		//capturamos el dato del octal
 
-middle=Integer.parseInt(oct,8); 		//se convierte del numero binario a decimal
-hex=Integer.toHexString(middle);		//aqui convertimos del decimal al octal
+middle=Integer.parseInt(oct,8); 		//se convierte del numero oct a decimal
+hex=Integer.toHexString(middle);		//aqui convertimos del decimal al hexadecimal
 
-System.out.println("El numero decimal es: "+ hex); //Aqui mismo se hace la convercion y se imprime
+System.out.println("El numero hexadecimal es: "+ hex); //Aqui se imprime el hexadecimal
 }
 }
