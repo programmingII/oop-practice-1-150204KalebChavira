546a294dcf131527f7bee31bf63bdfd61145e5bc
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-17 19:04:25 -0700

    Update Ejercicio26_convoctbin.java

--- OOP1/Ejercicio26_convoctbin.java
+++ OOP1/Ejercicio26_convoctbin.java
@@ -7,15 +7,15 @@ public class Ejercicio26_convoctbin{ 				//declaracion de la clase
 public static void main (String [ ] args){			//declaracion del programa principal 
 
 Scanner sc=new Scanner(System.in); //se crea el objeto de la clase Scanner para poder capturar datos desde el teclado
-String oct,bin;
-int middle;
+String oct,bin;//aqui se declaran los datos de la conversion
+int middle; // punto medion de la convesion para almacenar un decimal
 
-System.out.println("Ingrese el numero octal: "); //se imprime en pantalla la instruccion
-oct = sc.nextLine();		//capturamos el dato del binario
+System.out.println("Ingrese el numero octal: "); //se pide el numero octal
+oct = sc.nextLine();		//capturamos el dato del octal
 
-middle=Integer.parseInt(oct,8); 		//se convierte del numero binario a decimal
-bin=Integer.toBinaryString(middle);		//aqui convertimos del decimal al octal
+middle=Integer.parseInt(oct,8); 		//se convierte del numero octal a decimal
+bin=Integer.toBinaryString(middle);		//aqui convertimos del decimal al binario
 
-System.out.println("El numero decimal es: "+ bin); //Aqui mismo se hace la convercion y se imprime
+System.out.println("El numero binario es: "+ bin); //Aqui mismo se hace la convercion y se imprime
 }
 }
