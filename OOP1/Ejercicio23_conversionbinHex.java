8b1d843cc363ded40a0f59b7669566b000465e4f
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-17 18:15:44 -0700

    Add files via upload

---
+++ OOP1/Ejercicio23_conversionbinHex.java
@@ -0,0 +1,21 @@
+/*Name: Joaquin Kaleb Chavira Gonzalez
+Date: 17/02/19
+Hour: 5:20*/
+import java.util.Scanner; //Importamos la libreria Scanner
+
+public class Ejercicio23_conversionbinHex{ 				//declaracion de la clase 
+public static void main (String [ ] args){			//declaracion del programa principal 
+
+Scanner sc=new Scanner(System.in); //se crea el objeto de la clase Scanner para poder capturar datos desde el teclado
+String bin,hexa;
+int middle;
+
+System.out.println("Ingrese el numero binario: "); //se imprime en pantalla la instruccion
+bin = sc.nextLine();		//capturamos el dato del binario
+
+middle=Integer.parseInt(bin,2); 		//se convierte del numero binario a decimal
+hexa=Integer.toHexString(middle);		//aqui convertimos del decimal al hexadecimal
+
+System.out.println("El numero hexadecimal es: "+ hexa); //Aqui mismo se hace la convercion y se imprime
+}
+}
