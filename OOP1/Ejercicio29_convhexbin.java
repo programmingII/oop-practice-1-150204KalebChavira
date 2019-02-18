ff9d04986ad0d30041992a80e16a3c959a3bca89
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-17 19:09:27 -0700

    Update Ejercicio29_convhexbin.java

--- OOP1/Ejercicio29_convhexbin.java
+++ OOP1/Ejercicio29_convhexbin.java
@@ -7,15 +7,15 @@ public class Ejercicio29_convhexbin{ 				//declaracion de la clase
 public static void main (String [ ] args){			//declaracion del programa principal 
 
 Scanner sc=new Scanner(System.in); //se crea el objeto de la clase Scanner para poder capturar datos desde el teclado
-String hex,bin;
-int middle;
+String hex,bin;//vars de la conversion
+int middle;//punto medio en decimal
 
-System.out.println("Ingrese el numero hexadecimal: "); //se imprime en pantalla la instruccion
-hex= sc.nextLine();		//capturamos el dato del binario
+System.out.println("Ingrese el numero hexadecimal: "); //se pide el hexa
+hex= sc.nextLine();		//capturamos el dato del hexa
 
-middle=Integer.parseInt(hex,16); 		//se convierte del numero binario a decimal
-bin=Integer.toBinaryString(middle);		//aqui convertimos del decimal al octal
+middle=Integer.parseInt(hex,16); 		//se convierte del numero hexa a decimal
+bin=Integer.toBinaryString(middle);		//aqui convertimos del decimal al binario
 
-System.out.println("El numero binario es: "+ bin); //Aqui mismo se hace la convercion y se imprime
+System.out.println("El numero binario es: "+ bin); //Aqui  se imprime el binario
 }
 }
