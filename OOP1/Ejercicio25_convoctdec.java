ad835e658094ffc34888ba29cb6ab762bee90aab
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-17 19:02:18 -0700

    Update Ejercicio25_convoctdec.java

--- OOP1/Ejercicio25_convoctdec.java
+++ OOP1/Ejercicio25_convoctdec.java
@@ -7,15 +7,14 @@ public class Ejercicio25_convoctdec{ 				//declaracion de la clase
 public static void main (String [ ] args){			//declaracion del programa principal 
 
 Scanner sc=new Scanner(System.in); //se crea el objeto de la clase Scanner para poder capturar datos desde el teclado
-String oct;
+String oct; // string del octal
 int middle;
 
-System.out.println("Ingrese el numero octal: "); //se imprime en pantalla la instruccion
-oct = sc.nextLine();		//capturamos el dato del binario
+System.out.println("Ingrese el numero octal: "); //aqui se pide el numero octal
+oct = sc.nextLine();		//capturamos el dato del octal
 
 middle=Integer.parseInt(oct,8); 		//se convierte del numero binario a decimal
-//oct=Integer.toOctalString(middle);		//aqui convertimos del decimal al octal
 
-System.out.println("El numero decimal es: "+ middle); //Aqui mismo se hace la convercion y se imprime
+System.out.println("El numero decimal es: "+ middle); //Aqui se imprime el decimal
 }
 }
