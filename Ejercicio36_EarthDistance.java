4faab989bbd139aab9859742c99d3047132448bf
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-24 18:59:55 -0700

    Add files via upload

---
+++ Ejercicio36_EarthDistance.java
@@ -0,0 +1,23 @@
+/*Name: Joaquin Kaleb Chavira Gonzalez
+Date: 24/02/19
+Hour: 6:50*/
+
+import java.util.Scanner;		//Importamos la libreria del Scanner
+
+class Ejercicio36_EarthDistance{		//inicio de la clase
+public static void main(String [] args){//inicio del main
+
+Scanner sc = new Scanner(System.in);	//Creamos un objeto Sc para capturar datos del teclado
+doble y1,y2,x1,x2;
+
+System.out.print("Ingresa la latitud de la Cordenada 1: ");
+y1 = input.nextDouble();
+System.out.print("Ingreda la longitud del la Cordenada 1: ");
+x1 = input.nextDouble();
+System.out.print("Ingresa la latitud de la Cordenada 2: ");
+y2 = input.nextDouble();
+System.out.print("Ingreda la longitud del la Cordenada 2: ")
+x2 = input.nextDouble();
+
+}		//salida del main 
+}		//salida de la clase
