5cfa8c0ba5bf312d3599b297fb2832b42d56be63
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-25 11:07:46 -0700

    Add files via upload

---
+++ OOP1/Ejercicio41_busascii.java
@@ -0,0 +1,15 @@
+/*Name: Joaquin Kaleb Chavira Gonzalez
+Date: 25/02/19
+Hour: 10:55*/
+
+
+
+class Ejercicio41_busascii{		//inicio de la clase
+public static void main(String [] args){//inicio del main
+
+char ascii = 'K';//aqui declaras el simbolo
+int num = (int)ascii;//aqui busca el valor del simbolo de ascii y lo guarda en la variable num
+System.out.println("El valor de " + ascii + " en ascii es :" + num); //salida del valor numerico del ascii
+
+}		//salida del main 
+}		//salida de la clase
