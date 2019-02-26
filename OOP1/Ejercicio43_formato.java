1661f025d102353b3d5d550e2298036eeecbcefc
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-26 12:55:07 -0700

    Add files via upload

---
+++ OOP1/Ejercicio43_formato.java
@@ -0,0 +1,14 @@
+/*Name: Joaquin Kaleb Chavira Gonzalez
+Date: 25/02/19
+Hour: 10:55*/
+
+import java.io.Console;
+
+class Ejercicio43_formato{		//inicio de la clase
+public static void main(String [] args){//inicio del main
+System.out.println("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\tUp above the world so high, \n\tLike a diamond in the sky. \n\tTwinkle, twinkle, little star, \n\tHow I wonder what you are!\n\n");
+//Salida donde se intenta aplicar el salto de linea y el tabular
+
+
+}		//salida del main 
+}		//salida de la clase
