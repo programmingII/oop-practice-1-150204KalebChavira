a8da29f02b1604a2773270e91478a8334b52b727
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-27 14:52:06 -0700

    Add files via upload

---
+++ OOP1/Ejercicio56_rango.java
@@ -0,0 +1,19 @@
+/*Name: Joaquin Kaleb Chavira Gonzalez
+Date: 27/02/19
+Hour: 2:44*/
+
+class Ejercicio56_rango	{	//inicio de la clase
+public static void main(String [] args){//inicio del main
+
+ int a=1;
+ int b=20;
+ int c=5;
+ int cont=0;
+   
+  for(int i=a; i<=b; i++){
+ if(i%c==0)
+ cont++;
+ }
+  System.out.println(cont);
+}		//salida del main 
+}		//salida de la clase
