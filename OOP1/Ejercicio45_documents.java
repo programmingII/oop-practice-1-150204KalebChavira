eb0230e7127fc2dd2ee60dda5cc97dc7b3659a4b
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-27 12:13:52 -0700

    Update Ejercicio45_documents.java

--- OOP1/Ejercicio45_documents.java
+++ OOP1/Ejercicio45_documents.java
@@ -7,7 +7,7 @@ import java.io.File;
 class Ejercicio45_documents	{	//inicio de la clase
 public static void main(String [] args){//inicio del main
 
-  System.out.println("/home/students/abc.txt  : " + new File("abc.txt").length() + " bytes");
-  System.out.println("/home/students/test.txt : " + new File("test.txt").length() + " bytes");
+  System.out.println("/home/students/abc.txt  : " + new File("abc.txt").length() + " bytes");//busca el documento .txt y reenvia la cantidad de bytes que tiene
+  System.out.println("/home/students/test.txt : " + new File("test.txt").length() + " bytes");//busca el documento .txt y reenvia la cantidad de bytes que tiene
 }		//salida del main 
 }		//salida de la clase
