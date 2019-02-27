857086196fc31d4ec374607f210a964429318a99
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-27 12:49:49 -0700

    Add files via upload

---
+++ OOP1/Ejercicio48_Lista.java
@@ -0,0 +1,14 @@
+/*Name: Joaquin Kaleb Chavira Gonzalez
+Date: 27/02/19
+Hour: 12:27*/
+
+
+class Ejercicio48_Lista	{	//inicio de la clase
+public static void main(String [] args){//inicio del main
+
+for(int i=1; i<=99; i++){//for que va i desde 1 hasta 99
+System.out.println(i);//imprime i
+}
+
+}		//salida del main 
+}		//salida de la clase
