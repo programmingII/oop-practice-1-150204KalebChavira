94beaaeadbc4baad7e2f4dc617f2a00da96caaa6
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-27 12:11:46 -0700

    Add files via upload

---
+++ OOP1/Ejercicio45_documents.java
@@ -0,0 +1,13 @@
+/*Name: Joaquin Kaleb Chavira Gonzalez
+Date: 25/02/19
+Hour: 10:55*/
+
+import java.io.File;
+
+class Ejercicio45_documents	{	//inicio de la clase
+public static void main(String [] args){//inicio del main
+
+  System.out.println("/home/students/abc.txt  : " + new File("abc.txt").length() + " bytes");
+  System.out.println("/home/students/test.txt : " + new File("test.txt").length() + " bytes");
+}		//salida del main 
+}		//salida de la clase
