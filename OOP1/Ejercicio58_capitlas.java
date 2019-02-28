58c64ca8010e3f2228fccc97375b9b5c061a49f4
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-27 17:28:54 -0700

    Add files via upload

---
+++ OOP1/Ejercicio58_capitlas.java
@@ -0,0 +1,25 @@
+/*Name: Joaquin Kaleb Chavira Gonzalez
+Date: 27/02/19
+Hour: 2:53*/
+
+
+import java.util.Scanner;
+class Ejercicio58_capitlas{	//inicio de la clase
+public static void main(String [] args){//inicio del main
+
+Scanner sc = new Scanner(System.in);
+
+System.out.print("Ingrese la frase:");
+String isim =sc.nextLine();
+
+String substring="";
+Scanner line = new Scanner(isim);
+
+while(line.hasNext()){
+String yenikelime =line.next();
+substring+= Character.toUpperCase( yenikelime.charAt(0))+yenikelime.substring(1)+" ";
+
+}
+System.out.println(substring.trim());
+}		//salida del main 
+}		//salida de la clase
