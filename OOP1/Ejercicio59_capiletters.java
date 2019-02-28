817f2f397adf6b21ffbd9ca930c2dd1483f47925
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-02-27 17:38:32 -0700

    Add files via upload

---
+++ OOP1/Ejercicio59_capiletters.java
@@ -0,0 +1,16 @@
+/*Name: Joaquin Kaleb Chavira Gonzalez
+Date: 27/02/19
+Hour: 5:33*/
+
+
+import java.util.Scanner;
+class Ejercicio59_capiletters{	//inicio de la clase
+public static void main(String [] args){//inicio del main
+
+    Scanner sc = new Scanner(System.in);
+     System.out.print("Ingrese la frase: ");
+	 String Linea = sc.nextLine();
+	 Linea = Linea.toLowerCase();
+	 System.out.println(Linea); 
+}		//salida del main 
+}		//salida de la clase
