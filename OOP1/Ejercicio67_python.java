55594c1c0d76bb669802ab9e23a51c3ced5d52d6
150204KalebChavira <46726393+150204KalebChavira@users.noreply.github.com>
2019-03-04 15:34:31 -0700

    Add files via upload

---
+++ OOP1/Ejercicio67_python.java
@@ -0,0 +1,17 @@
+/* Name: Joaquin Kaleb Chavira Gonzalez 
+Date: 04/03/2019
+Hour: 3:20*/
+
+public class Ejercicio67_python{
+public static void main(String[] args){
+
+String str="Python 3.0";  // se iniciliza la variable str;
+ String addition="Tutorial"; // se inicializa la variable addition;
+
+ String newpalabra= str.substring(0,7) +addition +str.substring(6); 
+ // inicializacion de la variable newpalabra
+
+System.out.println("Resultado: "+newpalabra); // se imprime en pantalla lo que esta entre parentesis
+
+}
+}
